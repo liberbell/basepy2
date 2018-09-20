64abc578c964584cc3622ed7ae597debc99727cc
liberbell <liberbell@gmail.com>
2018-09-20 09:01:04 +0900

    func7

--- func.py
+++ func.py
@@ -8,11 +8,18 @@ def func2(arg1, arg2):
 def cube(x):
     return x*x*x
 
+def power(num, x=1):
+    result = 1
+    for i in range(x):
+        result = result * num
+    return result
 
-func1()
-print(func1())
-print(func1)
+# func1()
+# print(func1())
+# print(func1)
 
 func2(10, 20)
 print(func2(10, 20))
 print(cube(3))
+
+power(10)
