bd26d52c751d6a28f83c2205e88b244878225e13
liberbell <liberbell@gmail.com>
2018-09-21 07:49:38 +0900

    cond6

--- conditional.py
+++ conditional.py
@@ -1,16 +1,18 @@
 
 def main():
-    x, y = 100, 100
+    x, y = 100, 10
     print('x: ', x, 'y: ', y)
 
-    if (x < y):
-        st = 'x is less than y'
-    elif(x > y):
-        st = 'x is greater than y'
-    elif(x == y):
-        st = 'x is same with y'
-    else:
-        st = 'It is damm status'
+    # if (x < y):
+    #     st = 'x is less than y'
+    # elif(x > y):
+    #     st = 'x is greater than y'
+    # elif(x == y):
+    #     st = 'x is same with y'
+    # else:
+    #     st = 'It is damm status'
+
+    st = 'x is less than y' if (x<y) else 'x is greater than or the same as y'
 
     print(st)
 
