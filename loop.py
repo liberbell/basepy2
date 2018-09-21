cafe3b1b0314a1a5a68eddd2db4badb3ed35d0eb
liberbell <liberbell@gmail.com>
2018-09-21 19:06:32 +0900

    loop2

--- loop.py
+++ loop.py
@@ -1,16 +1,20 @@
 def main():
     x = 0
 
-    while (x<5):
-        print(x)
-        x = x + 1
-
+    # while (x<5):
+    #     print(x)
+    #     x = x + 1
+    #
+    # for x in range(5, 10):
+    #     print(x)
+    #
+    # days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+    # for d in days:
+    #     print(d)
     for x in range(5, 10):
+        if x == 9: break
         print(x)
 
-    days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-    for d in days:
-        print(d)
 
 if __name__ == '__main__':
     main()
