e82f098e8cb0b5267f95d52a5c0dc5e50bbe8129
liberbell <liberbell@gmail.com>
2018-09-22 12:35:05 +0900

    class1

---
+++ class.py
@@ -0,0 +1,11 @@
+class myClass:
+    def method1(self):
+        print('myClass method1')
+
+    def method2(self):
+        print('myClass method2')
+
+def main():
+
+if __name__ == '__main__':
+    main()
