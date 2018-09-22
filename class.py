7b46494b66c65e777e4ff077f9edf88f3996b6c8
liberbell <liberbell@gmail.com>
2018-09-22 12:44:00 +0900

    class6

--- class.py
+++ class.py
@@ -5,10 +5,21 @@ class myClass:
     def method2(self, someStrings):
         print('myClass method2 ' + someStrings)
 
+class anotherClass:
+    def method1(self):
+        print('anotherClass method1')
+
+    def method2(self, someStrings):
+        print('anotherClass method2 ' + someStrings)
+
 def main():
     c = myClass()
     c.method1()
     c.method2('This is a string')
+
+    d = anotherClass()
+    d.method1()
+    d.method2('Wow this is anoter string')
 
 if __name__ == '__main__':
     main()
