17cdc4f5af1176d428b7cdda87c8d6ed4e6f5dd6
liberbell <liberbell@gmail.com>
2018-09-24 14:46:45 +0900

    timedelta2

---
+++ timedelta.py
@@ -0,0 +1,16 @@
+from datetime import date
+from datetime import time
+from datetime import datetime
+from datetime import timedelta
+
+print(timedelta(days=365, hours=5, minutes=1))
+
+now = datetime.now()
+print('Today is: ' + str(now))
+
+# def main():
+#
+#
+#
+# if __name__ == '__main__':
+#     main()
