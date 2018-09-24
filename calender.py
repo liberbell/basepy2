74a815c120227a258d54ad057f9906d0ed347207
liberbell <liberbell@gmail.com>
2018-09-25 08:55:40 +0900

    calendar6

--- calender.py
+++ calender.py
@@ -4,5 +4,10 @@ c = calendar.TextCalendar(calendar.SATURDAY)
 # hc = calendar.HTMLCalendar(calendar.SUNDAY)
 # st = hc.formatmonth(2018, 9)
 # print(st)
-for i in c.itermonthdays(2018, 9):
-    print(i)
+# for i in c.itermonthdays(2018, 9):
+#     print(i)
+for name in calendar.month_name:
+    print(name)
+
+for day in calendar.day_name:
+    print(day)
