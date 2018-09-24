05688e5ee7447c1022f9b50e284d9cd938ef52a5
liberbell <liberbell@gmail.com>
2018-09-24 14:48:06 +0900

    timedelta3

--- timedelta.py
+++ timedelta.py
@@ -8,6 +8,8 @@ print(timedelta(days=365, hours=5, minutes=1))
 now = datetime.now()
 print('Today is: ' + str(now))
 
+print('One year from now it will be: ' + str(now + timedelta(days=365)))
+
 # def main():
 #
 #
