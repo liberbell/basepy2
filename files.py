f7286e313a9651ce5cf216fd3d09b8743b883ce9
liberbell <liberbell@gmail.com>
2018-09-25 09:16:34 +0900

    files5

--- files.py
+++ files.py
@@ -1,11 +1,14 @@
 def main():
     # f = open('textfile.txt', 'w+')
-    f = open('textfile.txt', 'a')
+    f = open('textfile.txt', 'r')
 
-    for i in range(10):
-        f.write('This is line ' + str(i) + '\r\n')
+    # for i in range(10):
+    #     f.write('This is line ' + str(i) + '\r\n')
 
-    f.close()
+    # f.close()
+    if f.mode == 'r':
+        contents = f.read()
+        print(contents)
 
 if __name__ == '__main__':
     main()
