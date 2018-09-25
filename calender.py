a25f24c41ec4b5a26a23d1b13233eeea8603db9c
liberbell <liberbell@gmail.com>
2018-09-25 09:04:16 +0900

    calendar7

--- calender.py
+++ calender.py
@@ -6,8 +6,21 @@ c = calendar.TextCalendar(calendar.SATURDAY)
 # print(st)
 # for i in c.itermonthdays(2018, 9):
 #     print(i)
-for name in calendar.month_name:
-    print(name)
+# for name in calendar.month_name:
+#     print(name)
+#
+# for day in calendar.day_name:
+#     print(day)
 
-for day in calendar.day_name:
-    print(day)
+print('Teem meeting will be on:')
+for m in range(1, 31):
+    cal = calendar.monthcalendar(2018, m)
+    weekone = cal[0]
+    weektwo = cal[1]
+
+    if weekone[calendar.FRIDAY] != 0:
+        meetday = weekone[calendar.FRIDAY]
+    else:
+        meetday = weektwo[calendar.FRIDAY]
+
+    print('%10s %2d' % (calendar.month_name[m], meetday))
