156a2b075b0d41f9a492d0f5cc4f76219961c677
liberbell <liberbell@gmail.com>
2018-09-27 08:48:52 +0900

    shell4

--- shell.py
+++ shell.py
@@ -7,8 +7,10 @@ def main():
         src = path.realpath('textfile.txt')
         dst = src + '.bak'
 
-        shutil.copy(src, dst)
-        shutil.copystat(src, dst)
+        # shutil.copy(src, dst)
+        # shutil.copystat(src, dst)
+
+        os.rename('textfile.txt', 'newfile.txt')
 
 if __name__ == '__main__':
     main()
