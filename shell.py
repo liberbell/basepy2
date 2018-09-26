c74a713436cfc6bfb5e95eb601d870780dfffced
liberbell <liberbell@gmail.com>
2018-09-27 08:45:33 +0900

    shell1

---
+++ shell.py
@@ -0,0 +1,13 @@
+import os
+from os import path
+import shutil
+
+def main():
+    if path.exists('textfile.txt'):
+        src = path.realpath('textfile.txt')
+        dst = src + '.bak'
+
+        shutil.copy(src, dst)
+
+if __name__ == '__main__':
+    main()
