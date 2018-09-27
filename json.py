8a9df20f7d1465257fc2fbb517f6df2ecf53f782
liberbell <liberbell@gmail.com>
2018-09-27 09:21:02 +0900

    json4

--- json.py
+++ json.py
@@ -6,5 +6,8 @@ def printResult(data):
 def main():
     urlDATA = 'http://earchquake.usgs.gov/earchquakes/feed/v1.0/summary/2.5'
 
+    weburl = urllib.request.urlopen(urlDATA)
+    print('Result code: ' + str(weburl.getcode()))
+
 if __name__ == '__main__':
     main()
