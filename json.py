88ccb0e5add110cbe7ad07559fdc36a1ae86bb56
liberbell <liberbell@gmail.com>
2018-09-27 09:19:30 +0900

    json2

---
+++ json.py
@@ -0,0 +1,10 @@
+import urllib.request
+
+def printResult(data):
+    theJSON = json.loads(data)
+
+def main():
+    urlDATA = 'http://earchquake.usgs.gov/earchquakes/feed/v1.0/summary/2.5'
+
+if __name__ == '__main__':
+    main()
