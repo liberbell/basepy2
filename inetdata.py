d27e82ee768f9e4b52cf8fd0d8f5e87ff64f5f97
liberbell <liberbell@gmail.com>
2018-09-27 09:10:06 +0900

    inet2

--- inetdata.py
+++ inetdata.py
@@ -5,6 +5,9 @@ def main():
     weburl = urllib.request.urlopen('https://www.google.com')
     print('Result code: ' + str(weburl.getcode()))
 
+    data = weburl.read()
+    print(data)
+
 
 if __name__ == '__main__':
     main()
