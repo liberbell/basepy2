ff90c764c37071cf6cc5aaab90fd5803aa46cabf
liberbell <liberbell@gmail.com>
2018-09-28 07:39:31 +0900

    json1

--- json.py
+++ json.py
@@ -1,10 +1,11 @@
 import urllib.request
+import json
 
 def printResult(data):
     theJSON = json.loads(data)
 
 def main():
-    urlDATA = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5'
+    urlDATA = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_day.geojson'
 
     weburl = urllib.request.urlopen(urlDATA)
     print('Result code: ' + str(weburl.getcode()))
