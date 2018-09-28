18b8d756f1ff1a38843a13bd278c99674a01f0e8
liberbell <liberbell@gmail.com>
2018-09-28 19:45:24 +0900

    json4

---
+++ jsondata.py
@@ -0,0 +1,25 @@
+import urllib.request
+import json
+
+def printResult(data):
+    theJSON = json.loads(data)
+
+    if 'titel' in theJSON['metadata']:
+        print(theJSON['metadata']['title'])
+
+    count = theJSON['metadata']['count']
+    print(str(count) + ' events recorded')
+
+def main():
+    urlDATA = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_day.geojson'
+
+    weburl = urllib.request.urlopen(urlDATA)
+    print('Result code: ' + str(weburl.getcode()))
+    if (weburl.getcode() == 200):
+        data = weburl.read()
+        printResult(data)
+    else:
+        print('Received error, can`t parse results')
+
+if __name__ == '__main__':
+    main()
