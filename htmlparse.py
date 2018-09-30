28095c3c22b3b8ebddf093e6e497fe5dd9f7e27a
liberbell <liberbell@gmail.com>
2018-09-30 11:57:33 +0900

    htmlparse3

--- htmlparse.py
+++ htmlparse.py
@@ -1,6 +1,10 @@
 from html.parser import HTMLParser
 
 class MyHTMLParser(HTMLParser)
+    def handle_comment(self, data):
+        print('Encountered comment: ', data)
+        pos = self.getpos()
+        print('\tAt line: ', pos[0], ' position ', pos[1])
 
 
 def main():
@@ -9,7 +13,7 @@ def main():
     if f.mode == 'r':
         contents = f.read()
         parser.feed(contents)
-        
+
 
 if __name__ == '__main__':
     main()
