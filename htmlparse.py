c290fe8f82779cb6ba522b76cb20c73d69da7e41
liberbell <liberbell@gmail.com>
2018-09-30 11:45:53 +0900

    htmlparse2

---
+++ htmlparse.py
@@ -0,0 +1,15 @@
+from html.parser import HTMLParser
+
+class MyHTMLParser(HTMLParser)
+
+
+def main():
+    parser = MyHTMLParser()
+    f = open('samplehtml.html')
+    if f.mode == 'r':
+        contents = f.read()
+        parser.feed(contents)
+        
+
+if __name__ == '__main__':
+    main()
