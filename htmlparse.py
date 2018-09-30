4367e1d6b7e2a744eedb19fb300b508b9b59b987
liberbell <liberbell@gmail.com>
2018-09-30 12:29:27 +0900

    htmlparse11

--- htmlparse.py
+++ htmlparse.py
@@ -16,7 +16,7 @@ class MyHTMLParser(HTMLParser):
         pos = self.getpos()
         print('\tAt line: ', pos[0], ' position ', pos[1])
 
-        if attr.__len() > 0:
+        if attr.__len__() > 0:
             print('\tAttributes:')
             for a in attrs:
                 print('\t', a[0], '=', 'a[1]')
