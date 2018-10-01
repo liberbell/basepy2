28bbd5a70cd8a023f902220075a11cf2bb3c79da
liberbell <liberbell@gmail.com>
2018-10-01 19:34:58 +0900

    xmlparse5

--- xmlparse.py
+++ xmlparse.py
@@ -12,5 +12,16 @@ def main():
     for skill in skills:
         print(skill.getAttribute('name'))
 
+    newSkill = doc.createElement('skill')
+    newSkill.setAttribute('name', 'jQuery')
+
+    doc.firstChild.appendChild(newSkill)
+    skills = doc.getElementsByTagName('skill')
+    print('%d skills: ' % skills.length)
+
+    for skill in skills:
+        print(skill.getAttribute('name'))
+
+
 if __name__ == '__main__':
     main()
