82c05c8c2be218991c2da93f61426620794751bc
liberbell <liberbell@gmail.com>
2018-10-01 19:31:05 +0900

    xmlparse4

--- xmlparse.py
+++ xmlparse.py
@@ -2,9 +2,12 @@ import xml.dom.minidom
 
 def main():
     doc = xml.dom.minidom.parse('samplexml.xml')
-    
+
     print(doc.nodeName)
     print(doc.firstChild.tagName)
+
+    skills = doc.getElementsByTagName('skill')
+    print('%d skills: ' % skills.length)
 
 if __name__ == '__main__':
     main()
